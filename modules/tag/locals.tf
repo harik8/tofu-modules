2395a3aaeb7c47668f778364d8620b658dd6847f
harik8 <khprasaad@gmail.com>
2024-09-08 11:35:32 +0200

    D-080924: remove region short code

--- modules/tag/locals.tf
+++ modules/tag/locals.tf
@@ -1,25 +1,14 @@
 locals {
-  base_name = join("-", [var.workload, local.region_short_codes[local.region], var.environment, var.utilization])
 
-  name = var.postfix == "" ? local.base_name : join("-", [local.base_name, var.postfix])
-
-  environment = var.environment == "" ? terraform.workspace : var.environment 
-
-  region = data.aws_region.current.name
-
-  region_short_codes = {
-    "eu-north-1" = "eun1",
-  }
+  environment = var.environment == "" ? terraform.workspace : var.environment
 
   default_tags = {
-    Name        = join("-", [var.workload, local.region_short_codes[local.region], local.environment, var.utilization])
-    Prefix      = join("-", [var.workload, local.region_short_codes[local.region], local.environment])
+    Name        = join("-", [var.workload, local.environment, var.utilization])
+    Prefix      = join("-", [var.workload, local.environment])
     CostCenter  = var.costcenter
     Description = var.description
     Environment = local.environment
-    Region      = local.region
     Owner       = var.owner
-    Postfix     = var.postfix
     Utilization = var.utilization
     Workload    = var.workload
     OpenTofu    = true
